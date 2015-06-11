04fea818d1f18a0f52a40a9d7c05b530a4cf28a8
Thom Hurks <thomhurks@gmail.com>
2015-06-11 23:46:37 +0200

    Calculate peak usage, the built-in variant is not reliable

--- MassifParser.py
+++ MassifParser.py
@@ -2,6 +2,7 @@ __author__ = 'Thom Hurks'
 
 import os
 import re
+import sys
 
 # Algorithm IDs:
 # 0 is semi-naive
@@ -11,15 +12,12 @@ import re
 # 4 is SSC12
 algorithmNames = ["SemiNaive", "Smart", "SSC1", "SSC2", "SSC12"]
 # Set this to True/False if you want readable algorithm names or just the IDs.
-outputAlgorithmNamesReadable = False
+outputAlgorithmNamesReadable = True
 
 # Regular expressions to parse the input lines.
 re_peak = re.compile('Detailed snapshots: \[.*?(?P<peak>\d+) \(peak\).*\]')
 re_new = re.compile('Output for algorithm (?P<algo>\d) on dataset (?P<dataset>.+) with (?P<threads>\d{1,2}) threads')
-
-# Generate a new regex dynamically.
-def get_re(lineNr):
-    return re.compile(str(lineNr) + '(?:\s+[0-9,]+){2}\s+(?P<data>[0-9,]+)(?:\s+[0-9,]+){2}')
+re_sample = re.compile('(?P<counter>\d+)(?:\s+[0-9,]+){2}\s+(?P<data>[0-9,]+)(?:\s+[0-9,]+){2}')
 
 # Use this to set input/output names and output extension.
 inputFileName = "massif_output.txt"
@@ -44,64 +42,58 @@ if os.path.isfile(inputFileName):
                 outputFile = open(outputFileNameFinal, 'x')
                 if outputFile.writable():
                     # Write the file header.
-                    outputFile.write('"Data"\t"Algorithm"\t"Nr Threads"\t"Heap Usage (B)"\n')
+                    outputFile.write('"Data"\t"Algorithm"\t"Nr Threads"\t"Heap Usage (B)"\t"Nr Samples"\n')
                     outputCreated = True
             except FileExistsError:
                 attemptCounter += 1
         # Start reading in the input file
         # Parsing state variables:
-        correctOutputFound = False
-        peakFound = False
-        re_line = None
         algo = None
         dataset = None
         threads = None
+        largestSample = -1
+        sampleCounter = -1
         for line in massifFile:
             lineCounter += 1
             # Useful content starts after header offset.
             if lineCounter > headerOffset:
                 # Basic state machine during parsing:
-                if correctOutputFound is False and peakFound is False:
-                    newOutput = re_new.search(line)
-                    if newOutput is not None:
-                        algo = newOutput.group('algo')
-                        dataset = newOutput.group('dataset')
-                        threads = newOutput.group('threads')
-                        if algo is not None and dataset is not None and threads is not None:
-                            correctOutputFound = True
-                            print('##############################################################################')
-                            print(line)
-                            continue
-                elif correctOutputFound is True and peakFound is False:
-                    newPeak = re_peak.search(line)
-                    if newPeak is not None:
-                        peak = newPeak.group('peak')
-                        if peak is not None:
-                            peakFound = True
-                            re_line = get_re(peak)
-                            print(line)
-                            continue
-                elif correctOutputFound is True and peakFound is True:
-                    dataLine = re_line.search(line)
-                    if dataLine is not None:
-                        memory = dataLine.group('data')
-                        if memory is not None:
-                            # Reset state.
-                            correctOutputFound = False
-                            peakFound = False
-                            re_line = None
-                            print(line)
-                            print("data used: " + memory)
+                newOutput = re_new.search(line)
+                if newOutput is not None:
+                    temp_algo = newOutput.group('algo')
+                    temp_dataset = newOutput.group('dataset')
+                    temp_threads = newOutput.group('threads')
+                    if temp_algo is not None and temp_dataset is not None and temp_threads is not None:
+                        if algo is not None and dataset is not None and threads is not None and largestSample > 0 \
+                                and sampleCounter > -1:
                             if outputAlgorithmNamesReadable:
                                 algo = algorithmNames[int(algo)]
-                            outputFile.write('\"' + dataset + '\"\t\"' + algo + '\"\t\"' + threads + '\"\t\"' + memory + '\"\n')
-                            # Clear data after writing.
-                            algo = None
-                            dataset = None
-                            threads = None
-                            memory = None
-                else:
-                    print("Error on line " + str(lineCounter))
+                            outputFile.write('\"' + dataset + '\"\t\"' + algo + '\"\t\"' + threads + '\"\t\"' +
+                                             str(largestSample) + '\"\t\"' + str(sampleCounter) + '\"\n')
+                            print(largestSample)
+                        algo = temp_algo
+                        dataset = temp_dataset
+                        threads = temp_threads
+                        largestSample = -1
+                        sampleCounter = -1
+                        print('##############################################################################')
+                        print(line)
+                        continue
+                newSample = re_sample.search(line)
+                if newSample is not None:
+                    currentCount = newSample.group('counter')
+                    dataSample = newSample.group('data')
+                    if currentCount is not None and dataSample is not None:
+                        currentCount = int(currentCount)
+                        dataSample = int(dataSample.replace(',', ''))
+                        if currentCount == sampleCounter + 1:
+                            if dataSample > largestSample:
+                                largestSample = dataSample
+                            sampleCounter = currentCount
+                            continue
+                        else:
+                            sys.exit("Samples are not in order!")
+        print(largestSample)
         outputFile.close()
         print("Done parsing " + str(lineCounter) + " lines. Output written to " + outputFileNameFinal)
 else:
